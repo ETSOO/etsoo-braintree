757ce5adcfb06324bf4d42f4fcf171e27653059b
Garry Xiao <xz@etsoo.com>
2023-08-08 10:55:09 +1200

    For debug 2

--- src/EtsooBraintree.tsx
+++ src/EtsooBraintree.tsx
@@ -560,7 +560,7 @@ async function createPaypal(
 
     fundingSourceItems.forEach((fundingSource) => {
       // The Vault flow does not support Pay Later offers
-      if (vaultOnly && fundingSource === "paylater") return;
+      // if (vaultOnly && fundingSource === "paylater") return;
 
       try {
         const style =
